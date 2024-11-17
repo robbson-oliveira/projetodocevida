c87284d8e38fc4e657cfefccf061317137f4abfe
Robson Oliveira <robsonempdigital@gmail.com>
2024-11-17 17:51:24 -0300

    Entrega e Apresentação

---
+++ js/app.js
@@ -0,0 +1,613 @@
+$(document).ready(function () {
+  cardapio.eventos.init();
+});
+
+var cardapio = {};
+
+var MEU_CARRINHO = [];
+var MEU_ENDERECO = null;
+
+var VALOR_CARRINHO = 0;
+var VALOR_ENTREGA = 2;
+
+var CELULAR_EMPRESA = '5511968297926';
+
+
+cardapio.eventos = {
+  init: () => {
+    cardapio.metodos.obterItensCardapio();
+  }
+};
+
+cardapio.metodos = {
+  // Obtem a lista de itens do cardápio    
+  obterItensCardapio: (categoria ='cakes', varmais = false) => {
+    var filtro = MENU[categoria];
+    console.log(filtro);
+
+    if (!varmais) {
+      $("#itensCardapio").html('')
+      $("#btnVerMais").removeClass('hidden');
+    }
+
+
+    // Para cada item que ele passar irá criar um card
+    $.each(filtro, (i, e) => {
+
+      let temp = cardapio.templates.item
+      .replace(/\${img}/g, e.img)
+      .replace(/\${name}/g, e.name)
+      .replace(/\${price}/g, e.price.toFixed(2).replace('.', ','))
+      .replace(/\${id}/g, e.id)
+
+
+      // botão ver mais foi clicado (12 itens)
+      if (varmais && i >= 8 && i < 12) {
+          $("#itensCardapio").append(temp);
+      }
+
+      //paginação inicial (8 itens)
+      if (!varmais && i < 8) {
+          $("#itensCardapio").append(temp);
+
+      }
+
+    })
+      //remove o ativo
+      $(".container-menu a").removeClass('active')
+
+      //seta o menu para ativo
+      $("#menu-" + categoria).addClass('active')
+  },
+
+  //clique no botão de ver mais 
+  verMais: () => {
+    
+    var ativo = $(".container-menu a.active").attr('id').split('menu-')[1]; //menu-cakes
+    cardapio.metodos.obterItensCardapio(ativo, true)
+
+    $("#btnVerMais").addClass('hidden');
+  
+  },
+
+  // Diminuir a quantidade do item no cardápio
+  diminuirQuantidade: (id) => {
+
+    let qntdAtual = parseInt($("#qntd-" + id).text());
+
+    if (qntdAtual > 0) {
+      $("#qntd-" + id).text(qntdAtual - 1)
+    }
+
+  },
+  
+  // Aumentar a quantidade do item no cardápio
+  aumentarQuantidade: (id) => {
+
+    let qntdAtual = parseInt($("#qntd-" + id).text());
+      $("#qntd-" + id).text(qntdAtual + 1)
+    
+
+  },
+
+  // Adicionar ao carrinho o item do cardapio
+  adicionarAoCarrinho: (id) => {
+
+    let qntdAtual = parseInt($("#qntd-" + id).text());
+
+    if (qntdAtual > 0) {
+
+      //Obter a categoria ativa
+      var categoria = $(".container-menu a.active").attr('id').split('menu-')[1];
+
+      //Otenha a lista de itens
+      let filtro = MENU[categoria];
+
+      //Obtem o item
+      let item = $.grep(filtro, (e, i) => {return e.id == id});
+
+      if (item.length > 0) {
+
+          // Validar se já existe esse item no carrinho
+          let existe = $.grep(MEU_CARRINHO, (elem, index) => {return elem.id == id});
+
+          //Caso já exista o item no carrinho, só altera a quantidade
+          if (existe.length > 0) {
+              let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+              MEU_CARRINHO[objIndex].qntd = MEU_CARRINHO[objIndex].qntd + qntdAtual;
+          }
+          //Caso ainda não exista o item no carrinho, adiciona ele
+          else {
+            item[0].qntd = qntdAtual
+            MEU_CARRINHO.push(item[0])
+          }
+          
+          cardapio.metodos.mensagem('Item adicionado ao carrinho', 'green')
+          $("#qntd-" + id).text(0)
+          
+          cardapio.metodos.atualizarBadgeTotal();
+
+      }
+
+    }
+
+  },
+
+  //Atualizar o badge de totais dos botões "Meu Carrinho"
+  atualizarBadgeTotal: () => {
+
+    var total = 0;
+
+    $.each(MEU_CARRINHO, (i, e) => {
+        total += e.qntd;
+    })
+
+    if (total > 0) {
+        $(".botao-carrinho").removeClass('hidden')
+        $(".container-total-carrinho").removeClass('hidden')
+
+    }
+    else {
+        $(".botao-carrinho").addClass('hidden')
+        $(".container-total-carrinho").addClass('hidden')
+    }
+
+    $(".badge-total-carrinho").html(total);
+
+  },
+
+  //Abrir a modal de carrinho
+  abrirCarrinho: (abrir) => {
+
+    if (abrir) {
+        $("#modalCarrinho").removeClass('hidden');
+        cardapio.metodos.carregarCarrinho();
+    }
+    else {
+      $("#modalCarrinho").addClass('hidden');
+    }
+
+  },
+
+  //Altera o texto e exibe os botões das etapas
+  carregarEtapa: (etapa) => {
+
+    if (etapa == 1) {
+        $("#lblTituloEtapa").text('Seu carrinho:');
+        $("#itensCarrinho").removeClass('hidden');
+        $("#localdaEntrega").addClass('hidden');
+        $("#resumoCarrinho").addClass('hidden');
+  
+        $(".etapa").removeClass('active');
+        $(".etapa1").addClass('active');
+  
+        $("#btnEtapaPedido").removeClass('hidden');
+        $("#btnEtapaEndereco").addClass('hidden');
+        $("#btnEtapaResumo").addClass('hidden');
+        $("#btnVoltar").addClass('hidden');
+    }
+  
+    if (etapa == 2) {
+        // Código para etapa 2
+        $("#lblTituloEtapa").text('Endereço de Entrega:');
+        $("#itensCarrinho").addClass('hidden');
+        $("#localdaEntrega").removeClass('hidden');
+        $("#resumoCarrinho").addClass('hidden');
+  
+        $(".etapa").removeClass('active');
+        $(".etapa1").addClass('active');
+        $(".etapa2").addClass('active');
+  
+        $("#btnEtapaPedido").addClass('hidden');
+        $("#btnEtapaEndereco").removeClass('hidden');
+        $("#btnEtapaResumo").addClass('hidden');
+        $("#btnVoltar").removeClass('hidden');
+    }
+  
+    if (etapa == 3) {
+        // Código para etapa 3
+        $("#lblTituloEtapa").text('Resumo do Pedido:');
+        $("#itensCarrinho").addClass('hidden');
+        $("#localdaEntrega").addClass('hidden');
+        $("#resumoCarrinho").removeClass('hidden');
+  
+        $(".etapa").removeClass('active');
+        $(".etapa1").addClass('active');
+        $(".etapa2").addClass('active');
+        $(".etapa3").addClass('active');
+
+  
+        $("#btnEtapaPedido").addClass('hidden');
+        $("#btnEtapaEndereco").addClass('hidden');
+        $("#btnEtapaResumo").removeClass('hidden');
+        $("#btnVoltar").removeClass('hidden');
+    }
+  
+  },
+  
+  //Botão genérico para voltar etapa
+  voltarEtapa: () => {
+
+    let etapa = $(".etapa.active").length;
+    cardapio.metodos.carregarEtapa(etapa - 1)
+
+  },
+
+  //Carrega a lista de itens do carrinho
+  carregarCarrinho: () => {
+
+    cardapio.metodos.carregarEtapa(1);
+  
+    if (MEU_CARRINHO.length > 0) {
+  
+      $("#itensCarrinho").html('');
+  
+      $.each(MEU_CARRINHO, (i, e) => {
+  
+        let temp = cardapio.templates.itemCarrinho.replace(/\${img}/g, e.img)
+          .replace(/\${name}/g, e.name)
+          .replace(/\${price}/g, e.price.toFixed(2).replace('.', ','))
+          .replace(/\${id}/g, e.id)
+          .replace(/\${qntd}/g, e.qntd);
+  
+        $("#itensCarrinho").append(temp);
+
+        //Último item
+        if ((i + 1) == MEU_CARRINHO.length) {
+              cardapio.metodos.carregarValores();
+        }
+  
+      })
+  
+      } 
+      else {    
+        $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="ph ph-basket"></i>Seu carrinho está vazio.</p>');
+          cardapio.metodos.carregarValores();
+      }
+  
+  },
+  
+  
+  //Diminuir quantidade do item no carrinho
+  diminuirQuantidadeCarrinho: (id) => {
+
+    let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+
+    if (qntdAtual > 1) {
+        $("#qntd-carrinho-" + id).text(qntdAtual - 1);
+        cardapio.metodos.atualizarCarrinho(id, qntdAtual - 1);
+    }
+    else {
+        cardapio.metodos.removerItemCarrinho(id)
+    }
+
+  },
+
+  //Aumentar quantidade do item no carrinho
+  aumentarQuantidadeCarrinho: (id) => {
+
+    let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+    $("#qntd-carrinho-" + id).text(qntdAtual + 1);
+    cardapio.metodos.atualizarCarrinho(id, qntdAtual + 1);
+
+  },
+
+  //Botão remover item do carrinho
+  removerItemCarrinho: (id) => {
+
+    MEU_CARRINHO = $.grep(MEU_CARRINHO, (e, i ) => {return e.id != id });
+    cardapio.metodos.carregarCarrinho();
+
+    //Atualiza o botão carrinho com a quantidade atualizada
+    cardapio.metodos.atualizarBadgeTotal();
+
+    //Atualizar os valores do (R$) totais do carrinho
+    cardapio.metodos.carregarValores();
+
+  },
+
+  //Atualiza com a quantidade atual
+  atualizarCarrinho: (id, qntd) => {
+
+    let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+    MEU_CARRINHO[objIndex].qntd = qntd;
+
+    //Atualiza o botão carrinho com a quantidade atualizada
+    cardapio.metodos.atualizarBadgeTotal();
+
+    //Atualizar os valores do (R$) totais do carrinho
+    cardapio.metodos.carregarValores();
+
+  },
+
+  //Carrega os valores de SubTotal, Entrega e Total
+  carregarValores: () => {
+
+    VALOR_CARRINHO = 0;
+
+    $("lblSubTotal").text('R$ 0,00');
+    $("lblValorEntrega").text('+ R$ 0,00');
+    $("lblValorTotal").text('R$ 0,00');
+
+    $.each(MEU_CARRINHO, (i, e) => {
+
+        VALOR_CARRINHO += parseFloat(e.price * e.qntd);
+
+        if ((i + 1) == MEU_CARRINHO.length) {
+            $("#lblSubTotal").text(`R$ ${VALOR_CARRINHO.toFixed(2).replace('.', ',')}`);
+            $("#lblValorEntrega").text(`+ R$ ${VALOR_ENTREGA.toFixed(2).replace('.', ',')}`);
+            $("#lblValorTotal").text(`R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.', ',')}`);
+        }
+
+    })
+
+  },
+
+  //Carregar a etapa enderços
+  carregarEndereco: () => {
+
+    if (MEU_CARRINHO.length <= 0) {
+        cardapio.metodos.mensagem('Seu carrinho está vazio.')
+        return;    
+    }
+
+    cardapio.metodos.carregarEtapa(2);
+
+  },
+
+  //API ViaCEP
+  buscarCEP: () => {
+
+      //Cria a variavel com o valor do CEP
+      var cep = $("#txtCEP").val().trim().replace(/\D/g, '');
+
+      //Verifica se o CEP possui valor informado
+      if (cep != "") {
+
+          //Expressão regular para validar o CEP
+          var validacep = /^[0-9]{8}$/;
+
+          if (validacep.test(cep)) {
+
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+
+
+                  if (!("erro" in dados)){
+
+                      //Atualizar os campos com os valores retornados
+                      $("#txtEndereco").val(dados.logradouro);
+                      $("#txtBairro").val(dados.bairro);
+                      $("#txtCidade").val(dados.localidade);
+                      $("#ddlUF").val(dados.uf);
+                      $("#txtNumero").focus();                   
+
+                  }
+                  else {
+                    cardapio.metodos.mensagem('CEP não encontrado. Preencha as informações manualmente.');
+                    $("#txtEndereco").focus();   
+
+                  }
+
+              });
+
+          }
+          else {
+                cardapio.metodos.mensagem('Formato do CEP inválido.');
+                $("#txtCEP").focus();   
+
+          }
+
+      }
+      else {
+          cardapio.metodos.mensagem('Informe o CEP, por favor.');
+          $("#txtCEP").focus();   
+      }
+
+  },
+
+  //Validação antes de processeguir para a etapa 3
+  resumoPedido: () => {
+
+      let cep = $("#txtCEP").val().trim();
+      let endereco = $("#txtEndereco").val().trim();
+      let bairro = $("#txtBairro").val().trim();
+      let cidade = $("#txtCidade").val().trim();
+      let uf = $("#ddlUF").val().trim();
+      let numero = $("#txtNumero").val().trim();
+      let complemento = $("#txtComplemento").val().trim();
+
+      if (cep.length <= 0) {
+          cardapio.metodos.mensagem('Informe o CEP, por favor.');
+          $("#txtCEP").focus(); 
+          return;
+      }
+      if (endereco.length <= 0) {
+        cardapio.metodos.mensagem('Informe o endereço, por favor.');
+        $("#txtEndereco").focus(); 
+        return;
+      }
+      if (bairro.length <= 0) {
+        cardapio.metodos.mensagem('Informe o bairro, por favor.');
+        $("#txtCidade").focus();
+        return;
+      }
+      if (cidade.length <= 0) {
+        cardapio.metodos.mensagem('Informe o cidade, por favor.');
+        $("#txtBairro").focus();
+        return;
+      }
+      if (uf == -1) {
+        cardapio.metodos.mensagem('Informe a UF, por favor.');
+        $("#ddlUF").focus();
+        return;
+      }
+      if (numero.length <= 0) {
+        cardapio.metodos.mensagem('Informe o numero, por favor.');
+        $("#txtNumero").focus();
+        return;
+      }
+
+      MEU_ENDERECO = {
+          cep: cep,
+          endereco: endereco,
+          bairro: bairro,
+          cidade: cidade,
+          uf: uf,
+          numero: numero,
+          complemento: complemento,
+      }
+
+      cardapio.metodos.carregarEtapa(3);
+      cardapio.metodos.carregarResumo();
+
+  },
+
+  //Carrega a etapa de Resumo do pedido
+  carregarResumo: () => {
+
+      $("#listaItensResumo").html('');
+
+      $.each(MEU_CARRINHO, (i, e) => {
+
+        let temp = cardapio.templates.itemResumo.replace(/\${img}/g, e.img)
+        .replace(/\${name}/g, e.name)
+        .replace(/\${price}/g, e.price.toFixed(2).replace('.', ','))
+        .replace(/\${qntd}/g, e.qntd);
+        
+        $("#listaItensResumo").append(temp);
+
+      });
+
+      $("#resumoEndereco").html(`${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`);
+      $("#cidadeEndereco").html(`${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`);
+
+      cardapio.metodos.finalizarPedido();
+
+  },
+
+  // Atualiza o link do botão do WhatsApp
+  finalizarPedido: () => {
+
+      if (MEU_CARRINHO.length > 0 && MEU_ENDERECO != null) {
+
+        var texto = 'Olá, gostaria de fazer um pedido:';
+        texto += `\n*Itens do Pedido:*\n\n\${itens}`;
+        texto += '\n*Endereço de entrega:*';
+        texto += `\n${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`;
+        texto += `\n${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`;
+        texto += `\n\n*Total (com entrega): R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.', ',')}*`;
+
+        var itens = '';
+
+        $.each(MEU_CARRINHO, (i, e) => {
+
+            itens += `*${e.qntd}x* ${e.name} ...... R$ ${e.price.toFixed(2).replace('.', ',')} \n`;
+
+            //Último item
+            if ((i + 1) == MEU_CARRINHO.length) {
+
+              texto = texto.replace(/\${itens}/g, itens);
+
+              console.log(texto)
+
+              //Converte a URL
+              let encode = encodeURI(texto);
+              let URL = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`;
+
+              $("#btnEtapaResumo").attr('href', URL);
+
+            }
+
+        })
+
+    }
+
+},
+
+  //Mensagem do sistema
+  mensagem: (texto, cor = 'red', tempo = 3500) => {
+
+      let id = Math.floor(Date.now() * Math.random()).toString();
+
+      let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
+      $("#container-mensagens").append(msg);
+
+      setTimeout(() => {
+          $('#msg-' + id).removeClass('fadeInDown');
+          $('#msg-' + id).addClass('fadeOutUp');
+          setTimeout(() => {
+            $('#msg-' + id).remove();
+          }, 800)
+      }, tempo)
+
+  },
+
+}
+
+cardapio.templates = {
+  item: `
+    <div class="col-3 mb-5">
+      <div class="card card-item" id="\${id}">
+        <div class="img-produto">
+          <img src="\${img}" alt="">
+        </div>
+        <h5 class="title-produto text-center mt-4">
+          \${name}
+        </h5>
+        <p class="price-produto text-center">R$ \${price}</p>
+        <div class="add-carrinho">
+          <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="ph ph-minus"></i></span>
+          <span class="add-numero-itens" id="qntd-\${id}">0</span>
+          <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="ph ph-plus"></i></span>
+          <span class="btn btn-add" onclick="cardapio.metodos.adicionarAoCarrinho('\${id}')"><i class="ph ph-shopping-cart"></i></span>
+        </div>
+      </div>
+    </div>
+  `,
+
+  itemCarrinho: `
+      <div class="col-12 item-carrinho">
+          <div class="img-produto">
+            <img src="\${img}" />
+          </div>
+          <div class="dados-produtos">
+            <p class="title-produto"><b>\${name}</b></p>
+            <p class="price-produto"><b>R$ \${price}</b></p>
+          </div>
+          <div class="add-carrinho">
+              <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i class="ph ph-minus"></i></span>
+              <span class="add-numero-itens" id="qntd-carrinho-\${id}">\${qntd}</span>
+              <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="ph ph-plus"></i></span>
+              <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="ph ph-x-circle"></i></span>
+          </div>
+      </div>
+`,
+
+  itemResumo: `
+
+        <div class="col-12 item-carrinho resumo">
+            <div class="img-produto-resumo">
+              <img src="\${img}" />
+            </div>
+            <div class="dados-produto">
+                <p class="title-produto-resumo">
+                  <b>\${name}</b>
+                </p>
+                <p class="price-produto-resumo">
+                  <b>R$ \${price}</b>
+                </p>
+            </div>
+
+            <p class="quantidade-produto-resumo">
+              x <b>\${qntd}</b>
+            </p>
+        </div>
+  
+  
+  `,
+
+}
+
+
+
+
---
+++ js/dados.js
@@ -0,0 +1,237 @@
+var MENU = {
+    "cakes": [
+      {
+        "id": "brownie_cake",
+        "img": "./img/produto/brownie_cake.png",
+        "name": "Cake de brownie rico em chocolate",
+        "dsc": "Delicioso bolo de brownie com cobertura rica em chocolate, denso e fudgy.",
+        "price": 35.00
+      },
+      {
+        "id": "carrot_cake",
+        "img": "./img/produto/carrot_cake.png",
+        "name": "Cake de cenoura com cobertura",
+        "dsc": "Bolo de cenoura coberto com uma camada cremosa de cream cheese e nozes picadas.",
+        "price": 28.00
+      },
+      {
+        "id": "lemon_cake",
+        "img": "./img/produto/lemon_cake.png",
+        "name": "Cake de limão suave",
+        "dsc": "Bolo suave com sabor de limão e cobertura de glacê de limão suave.",
+        "price": 32.00
+      },
+      {
+        "id": "chocolate_lava_cake",
+        "img": "/img/produto/chocolate_lava_cake.png",
+        "name": "Chocolate Lava Cake",
+        "dsc": "Bolo de chocolate com recheio derretido, servido quente e irresistível.",
+        "price": 33.00
+      },
+      {
+        "id": "pineapple_cake",
+        "img": "/img/produto/pineapple_cake.png",
+        "name": "Cake de abacaxi fresco",
+        "dsc": "Bolo úmido de abacaxi, com pedaços caramelizados de abacaxi no topo.",
+        "price": 29.00
+      },
+
+
+      {
+        "id": "apple_cinnamon_cake",
+        "img": "/img/produto/apple_cinnamon_cake.png",
+        "name": "Cake de Maçã com Canela",
+        "dsc": "Bolo macio de maçã com um toque de canela, coberto com pedaços caramelizados de maçã.",
+        "price": 33.00
+      },
+      {
+        "id": "hazelnut_chocolate_cake",
+        "img": "/img/produto/hazelnut_chocolate_cake.png",
+        "name": "Cake de Avelã com Chocolate",
+        "dsc": "Bolo de chocolate com recheio cremoso de avelã e cobertura de ganache.",
+        "price": 39.00
+      },
+      {
+        "id": "lemon_blueberry_cake",
+        "img": "/img/produto/lemon_blueberry_cake.png",
+        "name": "Cake de Limão com Mirtilo",
+        "dsc": "Bolo refrescante de limão com mirtilos frescos e cobertura de glacê de limão.",
+        "price": 35.00
+      },
+      {
+        "id": "banana_walnut_cake",
+        "img": "/img/produto/banana_walnut_cake.png",
+        "name": "Cake de Banana com Nozes",
+        "dsc": "Bolo úmido de banana com pedaços de nozes, coberto com uma fina camada de mel.",
+        "price": 32.00
+      },
+      {
+        "id": "coffee_caramel_cake",
+        "img": "/img/produto/coffee_caramel_cake.png",
+        "name": "Cake de Café com Caramelo",
+        "dsc": "Bolo com sabor marcante de café e cobertura de caramelo, perfeito para amantes de café.",
+        "price": 37.00
+      },
+      {
+        "id": "pear_almond_cake",
+        "img": "/img/produto/pear_almond_cake.png",
+        "name": "Cake de Pera com Amêndoas",
+        "dsc": "Bolo de pera com amêndoas laminadas, proporcionando um sabor suave e textura crocante.",
+        "price": 34.00
+      },
+      {
+        "id": "raspberry_white_chocolate_cake",
+        "img": "/img/produto/raspberry_white_chocolate_cake.png",
+        "name": "Cake de Framboesa com Chocolate Branco",
+        "dsc": "Bolo suave de chocolate branco com framboesas frescas e cobertura cremosa.",
+        "price": 38.00
+      }
+
+
+
+
+      
+    ],
+    "cupcakes": [
+      {
+        "id": "choco_cup",
+        "img": "/img/produto/choco_cup.png",
+        "name": "Cupcake de chocolate",
+        "dsc": "Cupcake de chocolate com cobertura cremosa e raspas de chocolate.",
+        "price": 15.00
+      },
+      {
+        "id": "vanilla_cup",
+        "img": "/img/produto/vanilla_cup.png",
+        "name": "Cupcake de baunilha",
+        "dsc": "Cupcake clássico de baunilha com cobertura de creme de baunilha e confeitos.",
+        "price": 15.00
+      },
+      {
+        "id": "caramel_cup",
+        "img": "/img/produto/caramel_cup.png",
+        "name": "Cupcake de caramelo",
+        "dsc": "Cupcake de caramelo com cobertura cremosa de caramelo e toque de flor de sal.",
+        "price": 18.00
+      },
+      {
+        "id": "matcha_cup",
+        "img": "/img/produto/matcha_cup.png",
+        "name": "Cupcake de chá verde matcha",
+        "dsc": "Cupcake feito com chá verde matcha, com cobertura de matcha buttercream.",
+        "price": 18.00
+      }
+    ],
+    "donuts": [
+      {
+        "id": "choco_donut",
+        "img": "/img/produto/choco_donut.png",
+        "name": "Donut de chocolate",
+        "dsc": "Donut coberto com calda de chocolate e confeitos coloridos.",
+        "price": 12.00
+      },
+      {
+        "id": "sugar_donut",
+        "img": "/img/produto/sugar_donut.png",
+        "name": "Donut açucarado",
+        "dsc": "Donut tradicional coberto com açúcar granulado para um toque crocante.",
+        "price": 12.00
+      },
+      {
+        "id": "vanilla_donut",
+        "img": "/img/produto/vanilla_donut.png",
+        "name": "Donut de baunilha",
+        "dsc": "Donut de baunilha com cobertura de glacê branco e confeitos coloridos.",
+        "price": 14.00
+      },
+      {
+        "id": "caramel_donut",
+        "img": "/img/produto/caramel_donut.png",
+        "name": "Donut de caramelo",
+        "dsc": "Donut coberto com calda de caramelo e toque especial de flor de sal.",
+        "price": 15.00
+      },
+      {
+        "id": "cinnamon_donut",
+        "img": "/img/produto/cinnamon_donut.png",
+        "name": "Donut com canela",
+        "dsc": "Donut macio e coberto com açúcar e canela, proporcionando um toque aromático.",
+        "price": 13.00
+      }
+    ],
+    "cookies": [
+      {
+        "id": "choco_cookie",
+        "img": "/img/produto/choco_cookie.png",
+        "name": "Cookie de chocolate",
+        "dsc": "Cookie de chocolate intenso com pedaços de chocolate macios e derretidos.",
+        "price": 10.00
+      },
+      {
+        "id": "oat_cookie",
+        "img": "/img/produto/oat_cookie.png",
+        "name": "Cookie de aveia",
+        "dsc": "Cookie rústico de aveia, perfeito para um lanche saudável e saboroso.",
+        "price": 10.00
+      },
+      {
+        "id": "nut_cookie",
+        "img": "/img/produto/nut_cookie.png",
+        "name": "Cookie de nozes",
+        "dsc": "Cookie crocante com pedaços de nozes torradas.",
+        "price": 12.00
+      },
+      {
+        "id": "almond_cookie",
+        "img": "/img/produto/almond_cookie.png",
+        "name": "Cookie de amêndoas",
+        "dsc": "Cookie crocante com amêndoas laminadas, perfeito para quem aprecia um toque refinado.",
+        "price": 11.00
+      },
+      {
+        "id": "peanut_butter_cookie",
+        "img": "/img/produto/peanut_butter_cookie.png",
+        "name": "Cookie de pasta de amendoim",
+        "dsc": "Cookie de pasta de amendoim, crocante e com o sabor intenso de amendoim.",
+        "price": 12.00
+      }
+    ],
+    "macarons": [
+      {
+        "id": "vanilla_mac",
+        "img": "/img/produto/vanilla_mac.png",
+        "name": "Macaron de baunilha",
+        "dsc": "Macaron delicado com sabor suave de baunilha e recheio cremoso.",
+        "price": 20.00
+      },
+      {
+        "id": "choco_mac",
+        "img": "/img/produto/choco_mac.png",
+        "name": "Macaron de chocolate",
+        "dsc": "Macaron de chocolate com recheio intenso e suave ao mesmo tempo.",
+        "price": 22.00
+      },
+      {
+        "id": "berry_mac",
+        "img": "/img/produto/berry_mac.png",
+        "name": "Macaron de frutas vermelhas",
+        "dsc": "Macaron de frutas vermelhas, com um recheio cremoso e sabor azedinho.",
+        "price": 21.00
+      },
+      {
+        "id": "lemon_mac",
+        "img": "/img/produto/lemon_mac.png",
+        "name": "Macaron de limão",
+        "dsc": "Macaron de limão com recheio de creme cítrico, suave e refrescante.",
+        "price": 23.00
+      },
+      {
+        "id": "pistachio_mac",
+        "img": "/img/produto/pistachio_mac.png",
+        "name": "Macaron de pistache",
+        "dsc": "Macaron com sabor de pistache, delicado e com um recheio cremoso.",
+        "price": 24.00
+      }
+    ]
+  }
+  
